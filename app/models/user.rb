dfbc762558db6ead01c9e84bbe9884242bb6d448
Senay <senaialema@gmail.com>
2021-09-11 00:17:02 -0400

    User basic presence/uniqueness validations, password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { in: 8..20 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,38 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     before(:each) do
-      @user = User.new({first_name: 'Senay', last_name: 'Hailu', email: '1@1.com', password: '1234'})
+      @user = User.new({first_name: 'Senay', last_name: 'Hailu', email: '1@1.com', password: '123456789'})
     end
   
     it("Saves User when all fields are filled") do
       expect(@user.save).to eq true
     end
 
-    
+    it("Shouldn't save when firstname is nil") do
+      @user.first_name = nil
+      expect(@user.save).to eq false
+    end
+    it("Shouldn't save when lastname is nil") do
+      @user.last_name = nil
+      expect(@user.save).to eq false
+    end
+    it("Shouldn't save when email is nil") do
+      @user.email = nil
+      expect(@user.save).to eq false
+    end
+    it("Shouldn't save when email already exists") do
+      @user2 = User.new({first_name: 'Simon', last_name: 'Hailu', email: '1@1.com', password: '1234567890'})
+      @user2.save
+      expect(@user.save).to eq false
+    end
+    it("Shouldn't save when password is nil") do
+      @user.password = nil
+      expect(@user.save).to eq false
+    end
+    it("Shouldn't save when password length is not between 8 and 20") do
+      @user.password = '123'
+      expect(@user.save).to eq false
+    end
 
   end
 end
