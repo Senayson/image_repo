3aab3fd383d13dc0165df056eb61d195461f5e72
Senay <senaialema@gmail.com>
2021-09-11 00:38:40 -0400

    Create basic validations/tests for presence of fields title,size,category,url

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe Image, type: :model do
     it("Saves image when all fields are filled") do
       expect(@image.save).to eq true
     end
+    it("Shoudn't save image when title is nil") do
+      @image.title = nil
+      expect(@image.save).to eq false
+    end
+    it("Shoudn't save image when size is nil") do
+      @image.size = nil
+      expect(@image.save).to eq false
+    end
+    it("Shoudn't save image when category is nil") do
+      @image.category = nil
+      expect(@image.save).to eq false
+    end
+    it("Shoudn't save image when source url is nil") do
+      @image.url = nil
+      expect(@image.save).to eq false
+    end
 
     
   end
